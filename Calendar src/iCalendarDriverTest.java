bae32450ed4452d4298713dd1abab126f05091f0
Kevin Beydler <beydlerk@users.noreply.github.com>
2016-02-23 13:25:42 -1000

    jUnit tester for iCalendarDriver.java class

---
+++ Calendar src/iCalendarDriverTest.java
@@ -0,0 +1,20 @@
+/**
+ *The jUnit test class for the driver class for the Team Dois iCalendar project. 
+ * 
+ * @author Kevin Beydler, Chansen Hesia, Conner Higashino
+ * @version ICS 314
+ * @date 2/23/2016
+ **/
+
+//Import Preprocesser Directives
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+
+public class iCalendarDriverTest 
+{
+
+	
+	
+}
